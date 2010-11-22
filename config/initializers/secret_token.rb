8f67eb001fd1bb80457882a2fce63d860a82c172
Christian Fernandez <cfernandez@binaryfreedom.info>
2010-11-21 23:37:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevBinaryfreedomInfo::Application.config.secret_token = '256dc0c62a1eddb5352cfca6be66cdbd586b32ce8fd85eda403d97d68606afda1eccedcf6940fdb4c364d1954e6c34e5570a5b5376c96d9a81bbcabf6763fafe'
